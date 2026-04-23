0ff7f6e511473127368ed51da3204109c2580c7d
linazaouani <linazaouani13@gmail.com>
2026-04-23 19:36:17 +0100

    Update logo to the new version

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Logo = ({ className = "w-10 h-10" }) => {
   return (
     <img 
-      src="/logo_no_background.png" 
+      src="/logo.png" 
       alt="Dourous-Net Logo" 
       className={`${className} object-contain`}
     />
